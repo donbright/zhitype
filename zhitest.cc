7abfe03e614ceec19b81e9ff61e530a70d8e79c3
Don Bright <hugh.m.bright@gmail.com>
2015-05-08 23:28:25 -0500

    able to parse the glyf points by looking up glyf offset in loca table and parsing flags, xcoords, ycoords.

--- zhitest.cc
+++ zhitest.cc
@@ -67,7 +67,7 @@ Notes
 11. a lot of error checking has to be skipped (checksums for example)
 12. try not to use heap memory at all
 13. trade time for memory. this is why fseek() is everywhere.
-
+14. use 0b00000000 GCC binary literal extension
 
 Things Not Supported:
 
@@ -185,13 +185,14 @@ are given as... three points. Two 'on points' and one 'control point'. How
 does this work?
 
 
-Step 1. Find the directrix and focus of the Parabola. What's a directrix
-and focus? Go back to the Greeks and the 18th century school of Conics...
-and then come back.
+Step 1. Find the directrix and focus of the Parabola. Also the axis of
+symmetry. These are ideas from long ago, kind of predating the current
+rage for algebra uber alles.
 
 For any parabola, and two points on it, you can imagine it traces
 the trajectory of a ball thrown..... where the Force upon it, the gravity
-if you will, is directly perpendicular in dirction to the Directrix.
+if you will, is directly perpendicular in dirction to the Directrix..
+in parallel with the axis of symmetry.
 
 See? Now, of course, x cannot just be incremented by one anymore. . .
 and Y cannot be incremented by a simple velocity/acceleration addition. Why?
@@ -201,11 +202,44 @@ and y-axis plane. It will be facing maybe 30 degrees or whatever.
 But what we can do.... is look at the force-accelleration and the X velocity
 as vectors. Now, break them down into their x and y pieces to match our
 main coordinate system. For example if the directrix is heading on a slope
-from 0,0 to 3,4, then you can think of it as a vector with two parts. 
-(3,0) and (4,0). Now. We can add a fraction of this each loop and we will
-hopefuly get the resulsts sti.
+from 0,0 to 3,4, then you can think of it as a vector with two parts.
+(3,0) and (4,0). The Axis of Symmetry will then have slope as though
+it were from (0,0) to (4,-3). Thats how perpendicularity works.
+
+So ... in this case we would have not just y accelleration, but both
+x and y accelleration. And their proportion would be 4 to -3.
+A new loop:
+
+ x acceleration = 4
+ y acceleration = -3
+ loop
+   x position = x position + x velocity
+   x velocity = x velocity + x accelleration
+   y position = y position + y velocity
+   y velocity = y velocity + y accelleration
+   draw a point at x,y
+
+This will draw a 'rotated' parabola.
 
 
+
+But there is another question yet left unanswered. We are given
+three points... two on the parabola and one a 'control'. How to
+find our axis and accelleration given only these three points?
+
+Ah. Imagine the control point, forms a wedge with the line segments going
+to the other two points. Bisect this wedge. That is your axis of
+symmetry!
+
+But what is it's slope? It's rise and run?
+Ah. It is a combination of the slopes of the two line segments!
+
+xdiff1, ydiff1, xdiff2, ydiff2
+
+Imagine a parabola with an axis of symmetry that is straight up and down.
+Its x-diff will be 0. It's bisector will go straight down! The slope
+will be infinity. x = 0. Basically (x1-x0) + (x1-x2). The sum of the
+differences of the two vectors.
 
 
 
@@ -274,9 +308,10 @@ typedef struct fontinfo_t {
 	union uint32 ofascaler;
 	union uint16 numtables;
 	union uint32 cmap_table_offset;
-	union uint32 cmap_table_length;
 	union uint32 glyf_table_offset;
-	union uint32 glyf_table_length;
+	union uint32 loca_table_offset;
+	union uint32 head_table_offset;
+	union int16  head_table_indexToLocFormat;
 } fontinfo;
 
 // Part of main Font Directory, at beginning of file
@@ -301,11 +336,65 @@ void read_table_directories( fontinfo &fi, FILE * file ) {
 		read_table_directory(td,file);
 		if (equal(td.tag,"cmap")) {
 			fi.cmap_table_offset = td.offset;
-			fi.cmap_table_length = td.length;
 		} else if (equal(td.tag,"glyf")) {
 			fi.glyf_table_offset = td.offset;
-			fi.glyf_table_length = td.length;
+		} else if (equal(td.tag,"loca")) {
+			fi.loca_table_offset = td.offset;
+		} else if (equal(td.tag,"head")) {
+			fi.head_table_offset = td.offset;
 		}
+	}
+}
+
+void read_head_table( fontinfo &fi, FILE * f ) {
+	fseek( f, fi.head_table_offset.uint32, SEEK_SET ) ;
+	// Number Types: Fixed = 32 bit
+	// FWord 16 bit
+	// uFWord 16 bit
+	// longDateTime 64 bit
+
+	fseek( f, 4, SEEK_CUR ); //version
+	fseek( f, 4, SEEK_CUR ); //fontRevision
+	fseek( f, 4, SEEK_CUR ); //chuckSumAdjustment
+	//union uint32 tmp;      //magic number (for debug)
+	//read_uint32( tmp, f );
+	//printf("magic %08x\n",tmp.uint32); // sould be 0x5F0F3CF5
+	fseek( f, 4, SEEK_CUR ); //magic number
+	fseek( f, 2, SEEK_CUR ); //flags
+	fseek( f, 2, SEEK_CUR ); //unitsPerEm
+	fseek( f, 8, SEEK_CUR ); //time created
+	fseek( f, 8, SEEK_CUR ); //time modified
+	fseek( f, 2, SEEK_CUR ); //xmin
+	fseek( f, 2, SEEK_CUR ); //ymin
+	fseek( f, 2, SEEK_CUR ); //xmax
+	fseek( f, 2, SEEK_CUR ); //ymax
+	fseek( f, 2, SEEK_CUR ); //mac style
+	fseek( f, 2, SEEK_CUR ); //lowestRecPPEM
+	fseek( f, 2, SEEK_CUR ); //font diection hint
+	read_int16( fi.head_table_indexToLocFormat, f );
+	fseek( f, 2, SEEK_CUR ); //glyphDataFormat
+}
+
+// given an glyph index, look up the byte offset from the begin of glyf table.
+// for example, the 53rd glyph (index 52), could have a byte offset
+// from the beginning of the glyph-data-table of 0x0004304.
+// note there is no error checking for out of bounds.
+void lookup_glyf_offset( fontinfo &fi, union uint32 &glyf_index, union uint32 &glyf_offset, FILE *f ) {
+	fseek(f, fi.loca_table_offset.uint32, SEEK_SET);
+	if (fi.head_table_indexToLocFormat.int16==0) {
+		// LOC table is "short format".
+		// each element of LOC array is 2 bytes long
+		// and represents the offset in 16-bit words
+		fseek(f, glyf_index.uint32 * 2, SEEK_CUR );
+		union uint16 tmp;
+		read_uint16( tmp, f );
+		glyf_offset.uint32 = tmp.uint16 * 2;
+	} else {
+		// LOC table is "long format"
+		// each element of LOC array is 4 bytes long
+		// and represents the offset in 8-bit bytes
+		fseek(f, glyf_index.uint32 * 4, SEEK_CUR );
+		read_uint32( glyf_offset, f );
 	}
 }
 
@@ -327,14 +416,14 @@ typedef struct glyf_point_t {
 } glyf_point;
 
 // GF stands for 'Glyf Flag' - where the space-packing magic happens!
-#define GF_ON_CURVE   0b00000001;
-#define GF_XSHORT_VEC 0b00000010;
-#define GF_YSHORT_VEC 0b00000100;
-#define GF_REPEAT     0b00001000;
-#define GF_X_IS_SAME  0b00010000;
-#define GF_Y_IS_SAME  0b00100000;
-#define GF_POS_XSHORT 0b00010000;
-#define GF_POS_YSHORT 0b00100000;
+#define GF_ON_CURVE   0b00000001
+#define GF_XSHORT_VEC 0b00000010
+#define GF_YSHORT_VEC 0b00000100
+#define GF_REPEAT     0b00001000
+#define GF_X_IS_SAME  0b00010000
+#define GF_Y_IS_SAME  0b00100000
+#define GF_POS_XSHORT 0b00010000
+#define GF_POS_YSHORT 0b00100000
 
 void read_glyf_description( glyf_description &gd, FILE * f ) {
 	read_int16(gd.numberOfContours,f);
@@ -344,80 +433,166 @@ void read_glyf_description( glyf_description &gd, FILE * f ) {
 	read_int16(gd.yMax,f);
 }
 
-void read_coord( uint32_t fpos, int16_t &delta, FILE *f, bool isshort, bool issame ) {
-	fseek(f,fpos,SEEK_SET);
+void read_coord( uint32_t &offset, union int16 &delta, bool isshort, bool ispositive, FILE *f ) {
+	fseek(f,offset,SEEK_SET);
+	bool issame = ispositive; // two meanings, one number (see ttf docs)
 	if (isshort) {
+		uint8_t tmp;
 		read_uint8( tmp, f );
-		if (ispos) xdelta=tmp;
-		else xdelta=-tmp;
-	} else if (issame) {
-		xdelta = 0;
+		if (ispositive) delta.int16 = tmp; // cast 8bit to 16bit
+		else delta.int16 = -tmp;           // cast 8bit to 16bit
 	} else {
-		read_int16( xdelta, f );
+		if (issame) delta.int16 = 0;
+		else read_int16( delta, f );
+	}
+	offset = ftell(f);
+}
+
+void read_x_coord( union int16 &xdelta, uint8_t &flag, uint32_t &x_i, FILE *f ){
+	read_coord( x_i, xdelta, flag & GF_XSHORT_VEC, flag & GF_POS_XSHORT, f);
+}
+
+void read_y_coord( union int16 &ydelta, uint8_t &flag, uint32_t &y_i, FILE *f ){
+	read_coord( y_i, ydelta, flag & GF_YSHORT_VEC, flag & GF_POS_YSHORT, f);
+}
+
+void printglyfflag( uint8_t &f ); // forward declaration, here until refactor
+
+// read a flag. sounds easy but is complicated.
+// flags are sort of 'run length encoded' so you have to deal with repeats (runs)
+void readflag( uint8_t &flag, uint32_t &flags_i, uint8_t &repeat_counter, FILE *f ) {
+	if (repeat_counter>0) {
+		flag = flag;
+		repeat_counter--;
+	} else {
+		fseek(f,flags_i,SEEK_SET);
+		read_uint8( flag, f );
+		if (flag & GF_REPEAT) read_uint8(repeat_counter,f);
+		flags_i = ftell(f);
 	}
 }
 
-void do_glyf_data( glyf_description &gd, FILE *f, uint32_t dataoffset ){
-	fseek(f,dataoffset,SEEK_SET);
-	uint16_t num_points;
-	union uint16 endpt_index;
-	for (int i=0;i<gd.num_contours;i++) read_uint16( endpt_index, f );
-	num_points = endpt_index+1;
+// refacccctor
+void do_glyf_data( glyf_description &gd, FILE *f, uint32_t glyfdataoffset ){
+	// flag - explanation of 'xshort' and 'x-is-same/xshort-positive' bits.
+	// bit1 bit4 result (truth table)
+	// 1    0    x is 8bit value, sign is negative (9bit signed int)
+	// 1    1    x is 8bit value, sign is positive (9bit signed int)
+	// 0    0    x is 16 bit signed value
+	// 0    1    x is the same as the previous x coordinate
 
-	union uint16 instructionLength;
-	read_uint16( instructionLength, f );
-	uint32_t flags_i = ftell(f) + instructionLength * 1;
-	uint32_t endpts_i = endpts_offset;
-	glyf_point gp;
-	uint32_t x_i = flags_i;
-	uint32_t y_i = 0;
+	if (gd.numberOfContours.int16<0) return; // sient fail on compound glyfs
+	fseek(f,glyfdataoffset,SEEK_SET);
+	uint16_t num_points;
 
 	// step one. figure out the offsets where x coords begin and
 	// where y coords begin. how? read through all the flags.
 	// where the flags end, x coordinates begin. what about y?
 	// we can calculate how many bytes the x-coordinate list will use
-	// as we process the flags. so find that and add it to find y start.
+	// as we process the flags. where the x-coords end, y-coords begin
+	union uint16 endpt_index;
+	for (int i=0;i<gd.numberOfContours.int16;i++) {
+		read_uint16( endpt_index, f );
+		printf("contour %i endpt idx: %i\n", i, endpt_index.uint16);
+	}
+	num_points = endpt_index.uint16+1;
+	printf("total number of points: %i\n", num_points);
+
+	// skip "instructions" section
+	union uint16 instructionLength;
+	read_uint16( instructionLength, f );
+	fseek(f,instructionLength.uint16,SEEK_CUR);
+
+	// read every flag, calculate the last x-coordinate. (which
+	// is the beginning of the y coordinates). save index values (_i).
+	uint32_t endpts_i = glyfdataoffset;
+	uint32_t flags_offset = ftell(f);
+	uint32_t flags_i = ftell(f);
+	uint32_t x_i = ftell(f);
+	uint32_t y_i = ftell(f);
+	uint8_t xcoord_numbytes = 0;
 	uint8_t flag = 0;
 	uint8_t repeat_counter = 0;
-	bool done = false
+	uint16_t xcoordlist_numbytes = 0;
 	for (int i = 0; i < num_points; i++) {
-		if (repeat_counter>0)
-			flag = flag;
-			repeat_counter--;
-		} else {
-			read_uint8( flag, f );
-			if (flag & GF_REPEAT) read_uint8(repeat_counter,f);
-		}
-		uint8_t xcoord_numbytes = 2;
+		readflag( flag, flags_i, repeat_counter, f );
 		if (flag & GF_XSHORT_VEC) xcoord_numbytes = 1;
-		else if (flag & GF_X_IS_SAME) xcoord_numbytes = 0;
-		y_i += xcoord_numbytes;
+		else if (flag & GF_X_IS_SAME) xcoord_numbytes = 0; //xcoord_numbytes;
+		else xcoord_numbytes = 2;
+		xcoordlist_numbytes += xcoord_numbytes;
 	}
+	// now we are at end of flags.
+	// mark that this is where x coords start.
 	x_i = ftell(f);
-	y_i += x_i;
-	int16_t xcursor,ycursor;
-	int16_t xdelta,ydelta;
-	uint8_t tmp;
+	// we have calculated size of x coordinate list, and can calculate
+	// beginning of y coordinate list.
+	y_i = x_i + xcoordlist_numbytes;
+	// also reset the flags index
+	flags_i = flags_offset;
+
 	// step two. we have flag_i, x_i, y_i all set up. process the points.
-	for (int i=0;i<num_contours;i++){
-		fseek(f,flags_i,SEEK_SET);
-		read_uint8( flag, f );
-		read_coord( x_i, flag, xdelta, f, GF_XSHORT_VEC, GF_POS_XSHORT );
-		read_coord( y_i, flag, ydelta, f, GF_YSHORT_VEC, GF_POS_YSHORT );
+	int16_t xcursor,ycursor;
+	union int16 xdelta,ydelta;
+	glyf_point gp;
+	printf("idx endp %i flg %i x %i y %i\n",endpts_i,flags_i,x_i,y_i);
+	for (int i=0;i<gd.numberOfContours.int16;i++){
+		fseek( f, endpts_i, SEEK_SET );
+		read_uint16( endpt_index, f );
+		endpts_i = ftell(f);
+		repeat_counter = 0;
+		for (int j=0;j<endpt_index.uint16+1;j++) {
+			readflag( flag, flags_i, repeat_counter, f );
+			read_x_coord( xdelta, flag, x_i, f );
+			read_y_coord( ydelta, flag, y_i, f );
+			printf("yd %i\n",ydelta.int16);
+			printf("idx 4endp %i flg %i x %i y %i\n",endpts_i,flags_i,x_i,y_i);
+			printf("-\\\n");
+			printglyfflag( flag );
+			printf("xdelta %i \t ydelta %i \n", xdelta.int16, ydelta.int16 );
+			printf("-/\n");
+		}
 	}
 }
 
 #ifdef DEBUG
+void printglyfflag( uint8_t &f ){
+	printf("GF_ON_CURVE   %i\n", f & GF_ON_CURVE );
+	printf("GF_XSHORT_VEC %i\n", f & GF_XSHORT_VEC );
+	printf("GF_YSHORT_VEC %i\n", f & GF_YSHORT_VEC );
+	printf("GF_REPEAT     %i\n", f & GF_REPEAT );
+	if (f&GF_XSHORT_VEC) {
+		printf("GF_POS_XSHORT %i\n", f & GF_POS_XSHORT );
+		printf("GF_X_IS_SAME  n/a for 8bit coord\n" );
+	} else {
+		printf("GF_POS_XSHORT n/a for 16 bit coord\n" );
+		printf("GF_X_IS_SAME  %i\n", f & GF_X_IS_SAME );
+	}
+	if (f&GF_YSHORT_VEC) {
+		printf("GF_POS_YSHORT %i\n", f & GF_POS_YSHORT );
+		printf("GF_Y_IS_SAME  n/a for 8bit coord\n" );
+	} else {
+		printf("GF_POS_YSHORT n/a for 16 bit coord\n" );
+		printf("GF_Y_IS_SAME  %i\n", f & GF_Y_IS_SAME );
+	}
+}
+
 void printinfo( fontinfo &f ){
 	printf("ofascaler %u\n",f.ofascaler.uint32);
 	printf("numtables %u\n",f.numtables.uint16);
 	printf("cmap table offset hex %08x\n",f.cmap_table_offset.uint32);
-	printf("cmap table length %u\n",f.cmap_table_length.uint32);
 	printf("glyf table offset hex %08x\n",f.glyf_table_offset.uint32);
-	printf("glyf table length %u\n",f.glyf_table_length.uint32);
+	printf("loca table offset hex %08x\n",f.loca_table_offset.uint32);
+	printf("head table offset hex %08x\n",f.head_table_offset.uint32);
+	printf("head table indexToLocFormat %i\n",f.head_table_indexToLocFormat.int16);
+	if (f.head_table_indexToLocFormat.int16==0)
+		printf("  locformat: short (offsets=number of 16-bit words)\n");
+	else if (f.head_table_indexToLocFormat.int16==1)
+		printf("  locformat: long  (offsets=number of 8-bit bytes)\n");
+	else
+		printf("  locformat: unknown, not 0 or 1 \n");
 }
 
-void printglyf( glyf_description &gd ){
+void printglyfdescr( glyf_description &gd ){
 	printf("numberOfContours %i\n",gd.numberOfContours.int16);
 	printf("xMin %i\n",gd.xMin.int16);
 	printf("xMax %i\n",gd.xMax.int16);
@@ -448,14 +623,23 @@ int main(int argc, char * argv[]) {
 
 	read_table_directories( fi, file );
 
-	fseek(file,fi.glyf_table_offset.uint32,SEEK_SET);
+	read_head_table( fi, file );
+
+	union uint32 glyf_offset;
+	union uint32 glyf_index;
+	glyf_index.uint32 = 37;
+	lookup_glyf_offset( fi, glyf_index, glyf_offset, file );
+	printf("index, %u x%x, offset, x%x\n", glyf_index.uint32, glyf_index.uint32, glyf_offset.uint32 );
+	fseek( file, fi.glyf_table_offset.uint32, SEEK_SET);
+	printf("gt offqset, %x\n", fi.glyf_table_offset.uint32 );
+	fseek( file, glyf_offset.uint32, SEEK_CUR);
 	glyf_description gd;
 	read_glyf_description( gd, file );
+#ifdef DEBUG
+	printinfo( fi );
+	printglyfdescr( gd );
+#endif // DEBUG
 	do_glyf_data( gd, file, ftell(file) );
 
-#ifdef DEBUG
-	printglyf( gd );
-	printinfo( fi );
-#endif // DEBUG
 	return 0;
 }
